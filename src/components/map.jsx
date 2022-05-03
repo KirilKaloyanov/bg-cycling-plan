98207af717ddb4a126d98db44787e6f93da1eaff
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2022-05-03 11:03:23 +0300

    Fixed visualization of routes on the map.

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Polyline,
+  Marker,
+  Popup,
+  GeoJSON,
+} from "react-leaflet";
 import mapData from "../data/NatVelo.json";
 
 export default function Map() {
+  console.log(mapData.features[0].geometry.coordinates);
   return (
     <div className="map_container">
       <MapContainer center={[42.705, 25.09]} zoom={7.4} scrollWheelZoom={false}>
@@ -16,6 +24,11 @@ export default function Map() {
           </Popup>
         </Marker> */}
         <GeoJSON data={mapData.features} />
+        {/* <GeoJSON data={mapData.features[0].geometry.coordinates} /> */}
+        {/* <Polyline
+          pathOptions={{ color: "red" }}
+          positions={mapData.features[0].geometry.coordinates}
+        ></Polyline> */}
       </MapContainer>
     </div>
   );
