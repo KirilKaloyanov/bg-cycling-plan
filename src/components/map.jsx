0e61508ba141b1dd20d12d4e732276d92f38ee1f
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2022-05-04 11:22:32 +0300

    Added separate routes based on arrays of coordinates

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,16 +1,24 @@
 import React from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Polyline,
-  Marker,
-  Popup,
-  GeoJSON,
-} from "react-leaflet";
-import mapData from "../data/NatVelo.json";
+import { MapContainer, TileLayer, Polyline, Popup } from "react-leaflet";
+import ev13 from "../data/IronCurtainTrail10.js";
+import ev6 from "../data/DunavChernoMore2.js";
 
 export default function Map() {
-  console.log(mapData.features[0].geometry.coordinates);
+  function mapArrayToRoute(coordsData) {
+    let lineArray = [];
+    for (let key of coordsData.features) {
+      let coordsArray = [];
+      for (let coords of key.geometry.coordinates) {
+        coordsArray.push([coords[1], coords[0]]);
+      }
+      lineArray.push(coordsArray);
+    }
+    return lineArray;
+  }
+
+  let ev6Route = mapArrayToRoute(ev6);
+  let ev13Route = mapArrayToRoute(ev13);
+
   return (
     <div className="map_container">
       <MapContainer center={[42.705, 25.09]} zoom={7.4} scrollWheelZoom={false}>
@@ -18,17 +26,19 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <Marker position={[42.705, 25.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable. URaaa
-          </Popup>
-        </Marker> */}
-        <GeoJSON data={mapData.features} />
-        {/* <GeoJSON data={mapData.features[0].geometry.coordinates} /> */}
-        {/* <Polyline
-          pathOptions={{ color: "red" }}
-          positions={mapData.features[0].geometry.coordinates}
-        ></Polyline> */}
+
+        <Polyline
+          pathOptions={{ color: "blue", weight: 5 }}
+          positions={ev6Route}
+        >
+          <Popup>EuroVelo 6 - Дунавски велосипеден маршрут</Popup>
+        </Polyline>
+        <Polyline
+          pathOptions={{ color: "red", weight: 5 }}
+          positions={ev13Route}
+        >
+          <Popup>EuroVelo 13 - Пътят на Желязната завеса</Popup>
+        </Polyline>
       </MapContainer>
     </div>
   );
